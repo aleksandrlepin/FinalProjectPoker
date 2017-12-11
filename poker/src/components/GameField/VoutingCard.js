1496151e0bd8d831f7d92b732b844b8cc93c1efe
matveychuk <matveychuk.a@gmail.com>
2017-12-11 08:05:59 +0200

    check is user is exist

--- poker/src/components/GameField/VoutingCard.js
+++ poker/src/components/GameField/VoutingCard.js
@@ -3,17 +3,27 @@ import { socket } from '../../constants/consts';
 import openSocket from 'socket.io-client';
 import './voutingCard.css';
 
+
+
 export default class VoutingCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {voutingCardStyle: {}}
+    }
 
     handleChoiceNumber = (number) => {
         console.log(number);
+        this.setState({voutingCardStyle: {
+            backgroundColor: '#00ffff',
+            marginTop: '10px'
+        }}) 
         // socket.broadcast.emit('transferNumber', number);
         socket.emit('transferNumber', number);
         // chooseNumber(number);
     }
     render() {
         return (
-            <div className='voutingCard' onClick={this.handleChoiceNumber.bind(null, this.props.number)}>
+            <div style={this.state.voutingCardStyle} className='voutingCard' onClick={this.handleChoiceNumber.bind(null, this.props.number)}>
                 {this.props.number}
             </div>
         )
