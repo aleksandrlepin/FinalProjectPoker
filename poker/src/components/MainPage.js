261811d00347e0b5c60707d9d593e43e78ff5cd9
Dmitriy Amelchenko <dmitriyamelchenko@gmail.com>
2017-12-15 13:50:16 +0200

    added mainPage visual and work

--- poker/src/components/MainPage.js
+++ poker/src/components/MainPage.js
@@ -1,16 +1,28 @@
 import React from 'react';
-
+import './mainPage.css';
 // import Header from './Header/Header';
+import { withRouter } from 'react-router-dom';
 
 
 
-
-export default class MainPage extends React.Component {
+ class MainPage extends React.Component {
     render() {
         return (
-          <div className="container">
-main page
+          <div className="mainPage">
+              <div className="mainPageContent">
+
+                      <div className = "mainPageHeading">
+                        <h2>[Complex tasks through</h2>
+                          <h2>simple ways.</h2>
+                          <h2>Play, decide, do!_]</h2>
+                      </div>
+
+                  <button onClick={()=>{
+                        this.props.history.push("/login");
+                  }}>Start game</button>
+              </div>
         </div>
         )
     }
 }
+export default withRouter(MainPage);
