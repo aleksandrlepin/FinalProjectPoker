3b7c50c0d0a7fb6a8e76e414deee93c0cd407de3
Michael Korzun <31041089+MichaeIK@users.noreply.github.com>
2017-12-21 16:06:04 +0200

    Contacts + loader

---
+++ poker/src/components/Contacts/Contacts.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './contacts.css';
+import './loader.css';
+
+export default class Contacts extends Component {
+	state = {
+		users : ['MichaeIK', 'dmitriyamelchenko', 'Isabell07', 'Soverdo'],
+		usersRes : {}
+	}
+
+	componentDidMount() {
+
+		this.uploadData('matveychuk');
+	}
+
+	uploadData = (name) => {
+		fetch(`https://api.github.com/users/${name}`, { method: 'GET', headers: { "Content-Type": "application/json" }})
+		.then(res => res.json())
+		.then(res => {
+			if (res) {
+				let user = {
+					id: res.id,
+					login: res.login,
+					avatar_url: res.avatar_url,
+					html_url: res.html_url
+				}
+				this.setState({ usersRes: [...this.state.usersRes, user] });
+			}
+				if (this.state.usersRes.length < 5) this.uploadData(this.state.users[this.state.usersRes.length -1])
+		})
+		.catch(res => console.log('error', res));
+	}
+
+
+	render() {
+		console.log(this.state.usersRes)
+		return (
+			<div className="dev-wrap">
+
+			{this.state.usersRes.length > 2 ?
+				this.state.usersRes && this.state.usersRes.map((item, index) => {
+					return (
+						<div className="dev-card" key={index}>
+							<a className="dev-link" href={`https://github.com/${item.login}`}><h3 className="dev-login">{item.login}</h3></a>
+							<a className="dev-link" href={`https://github.com/${item.login}`}><img className="dev-avatars" src={item.avatar_url}></img></a>
+							<a className="dev-link" href={`https://github.com/${item.login}`}>{item.html_url}</a>
+						</div>
+					)
+				}) : <div className="loader-wrap">
+						<div className="loader">
+							<svg>
+								<defs>
+									<filter id="goo">
+										<feGaussianBlur in="SourceGraphic" stdDeviation="2" result="blur" />
+										<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 5 -2" result="gooey" />
+										<feComposite in="SourceGraphic" in2="gooey" operator="atop"/>
+									</filter>
+								</defs>
+							</svg>
+						</div>
+					</div>
+			}
+
+
+			</div>
+		);
+	}
+}
