976336d2422251dd963e0634307e90136cc44674
aleksandrlepin <yanektulho@gmail.com>
2018-05-08 15:12:05 +0300

    fix bugs with padding and margin for iphone 5

--- poker/src/index.js
+++ poker/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
-import 'font-awesome/css/font-awesome.min.css';
+// import 'font-awesome/css/font-awesome.min.css';
 
 // import { Provider } from 'react-redux';
 // import store from './store';
