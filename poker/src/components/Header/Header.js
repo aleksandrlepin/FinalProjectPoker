181be443ca001ddcc8133242fafd5228c89573d3
codeuri <yuriv@fanhubmedia.com>
2018-05-18 15:06:00 +0300

    Fix avatar displaying

--- poker/src/components/Header/Header.js
+++ poker/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import { socket } from '../../constants/consts';
 
 import { DBtoStore } from '../../actions';
 import store from '../GameField/store/index';
-// import {storage, storageRef} from '../../firebase';
+import {storage, storageRef} from '../../firebase';
 
 import go from '../../img/header/go.png';
 import it from '../../img/header/it.png';
@@ -12,112 +12,119 @@ import userpic from '../../img/header/user.jpg';
 
 
 class Header extends React.Component {
-    constructor() {
+	constructor() {
 
-        super();
-        this.state = { userpicUrl: '' }
+		super();
+		this.state = { userpicUrl: '' }
 
-        socket.on('userauth', (name) => {
-            console.log('LOooooooogggggiiiinnnn!!!!!!')
-            Header.getUserpic();
-            // // localStorage.setItem('playername', JSON.stringify(name));
-            // this.setState({ playername: name.toLocaleUpperCase() })
-            // // this.setState({playerName: JSON.parse(localStorage.getItem('playername'))})
+		// Это тут не нужно
+		// socket.on('login', (name) => {
+		//     console.log(arguments);
+		//     // localStorage.setItem('playername', JSON.stringify(name));
+		//     // this.setState({ playername: name.toLocaleUpperCase() })
+		//     // this.setState({playerName: JSON.parse(localStorage.getItem('playername'))})
+		//
+		//     console.log('socket from header', name, this.state.playername)
+		//     console.log('player', name);
+		// });
+	}
 
-            // console.log('socket from header', name, this.state.playername)
-            // console.log('player', name);
-        });
-    }
+	componentDidMount() {
+		this.getUserpic();
+	}
 
-    componentDidMount() {
-        // this.getUserpic();
-    }
+	getUserpic = () => {
+		const that = this;
+		const email = JSON.parse(localStorage.getItem('useremail'));
+		const name = JSON.parse(localStorage.getItem('username'));
 
-    // getUserpic = () => {
-    //     const that = this;
-    //     const email = JSON.parse(localStorage.getItem('useremail'));
-    //     const name = JSON.parse(localStorage.getItem('username'));
-    //     storage.refFromURL(`gs://pokergoit.appspot.com/userpics/${email}/${name}`).getDownloadURL().then(function(url) {
-    //         console.log('url', url);
-    //         that.setState({userpicUrl: url})
-    //     });
-    // }
+		//Добавил это
+		this.setState({userpicUrl : localStorage.getItem('avatar_url') });
 
-    handleClick = (href) => {
-        this.props.history.push(href);
-    }
+		//Компонент не отрендериться после того как получит ответ т.е это тоже не имеет смысла тут делать
+		//Перенесн это в файл Login.js
+		// storage.refFromURL(`gs://pokergoit.appspot.com/userpics/${email}/${name}`).getDownloadURL().then(function(url) {
+		//     console.log('url', url);
+		//     console.log(arguments);
+		//     // that.setState({userpicUrl: url})
+		// });
+	}
 
-    handleLogout = () => {
-        localStorage.clear();
-        this.props.history.push('/')
-    }
+	handleClick = (href) => {
+		this.props.history.push(href);
+	}
 
-    handleLogoClick = () => {
-        if (localStorage.getItem('isOwner')) {
-            this.props.history.push('/dashboard')
-        } else {
-            this.props.history.push('/')
-        }
-    }
-    handleRedirect = () => {
-        this.props.history.push('/dashboard')
-    }
-    handleClearUsers = () => {
-        let gameId = JSON.stringify({ gameId: JSON.parse(localStorage.getItem('gameId')) });
-        console.log(gameId)
-        fetch('/clearPlayers', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: gameId })
-            .then(res => res.json())
-            .then(res => {
-                store.dispatch(DBtoStore(res));
-                socket.emit('clearPlayers')
+	handleLogout = () => {
+		localStorage.clear();
+		this.props.history.push('/')
+	}
 
-            })
-            .catch(err => console.log(err))
-    }
+	handleLogoClick = () => {
+		if (localStorage.getItem('isOwner')) {
+			this.props.history.push('/dashboard')
+		} else {
+			this.props.history.push('/')
+		}
+	}
+	handleRedirect = () => {
+		this.props.history.push('/dashboard')
+	}
+	handleClearUsers = () => {
+		let gameId = JSON.stringify({ gameId: JSON.parse(localStorage.getItem('gameId')) });
+		console.log(gameId)
+		fetch('/clearPlayers', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: gameId })
+			.then(res => res.json())
+			.then(res => {
+				store.dispatch(DBtoStore(res));
+				socket.emit('clearPlayers')
 
-    render() {
-        // console.log('SOCET', socket);
-        return (
-            <header className="header">
-                <div className="header__container">
-                    <a onClick={this.handleLogoClick}>
-                        <img className="header__logo-go" src={go} alt="go" />
-                        <img className="header__logo-it" src={it} alt="it" />
-                    </a>
-                </div>
-                {localStorage.getItem('username') !== null
-                    ? <div className="header__container-link-and-img">
-                        <a  onClick={this.handleRedirect}>
-                            <div className="header__image-block">
-                                {this.props.userpicUrl && <img src={this.props.userpicUrl} alt="user" className="header__user-image" />}
-                            </div>
-                            <p className="header__profile">{JSON.parse(localStorage.getItem('username'))}</p>
-                        </a>
-                    </div>
-                    : null
-                }
-                {localStorage.getItem('username') !== null
-                    ? <div className="header__container-link-and-img">
-                        <div className="header__control">
-                            <a className="header__log-link" onClick={this.handleLogout}>
-                                Log Out
-                            </a>
-                        </div>
-                    </div>
-                    : <div className="header__container-link-and-img">
-                        <div className="header__control">
-                            <a className="header__register-link" onClick={this.handleClick.bind(null, '/registration')}>
-                                Register
-                            </a>
-                            <a className="header__log-link" onClick={this.handleClick.bind(null, '/login')}>
-                                Log In
-                            </a>
-                        </div>
-                    </div>
-                }
-            </header>
-        )
-    }
+			})
+			.catch(err => console.log(err))
+	}
+
+	render() {
+		// console.log('SOCET', socket);
+		return (
+			<header className="header">
+				<div className="header__container">
+					<a onClick={this.handleLogoClick}>
+						<img className="header__logo-go" src={go} alt="go" />
+						<img className="header__logo-it" src={it} alt="it" />
+					</a>
+				</div>
+				{localStorage.getItem('username') !== null
+					? <div className="header__container-link-and-img">
+						<a  onClick={this.handleRedirect}>
+							<div className="header__image-block">
+								{this.state.userpicUrl && <img src={this.state.userpicUrl} alt="user" className="header__user-image" />}
+							</div>
+							<p className="header__profile">{JSON.parse(localStorage.getItem('username'))}</p>
+						</a>
+					</div>
+					: null
+				}
+				{localStorage.getItem('username') !== null
+					? <div className="header__container-link-and-img">
+						<div className="header__control">
+							<a className="header__log-link" onClick={this.handleLogout}>
+								Log Out
+							</a>
+						</div>
+					</div>
+					: <div className="header__container-link-and-img">
+						<div className="header__control">
+							<a className="header__register-link" onClick={this.handleClick.bind(null, '/registration')}>
+								Register
+							</a>
+							<a className="header__log-link" onClick={this.handleClick.bind(null, '/login')}>
+								Log In
+							</a>
+						</div>
+					</div>
+				}
+			</header>
+		)
+	}
 }
 
 export default withRouter(Header)
--- poker/src/components/Login.js
+++ poker/src/components/Login.js
@@ -2,54 +2,64 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import LogError from './LogError';
 import { socket } from '../constants/consts';
+import {storage} from '../firebase';
+
 
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { logged: true }
-    }
+	constructor(props) {
+		super(props);
+		this.state = { logged: true }
+	}
 
-    handleLogIn = (e) => {
-        e.preventDefault();
-        let profile = JSON.stringify({
-            email: this.refs.email.value,
-            password: this.refs.password.value
-        })
-        fetch(`/login`, { method: 'POST', headers: { "Content-Type": "application/json" }, body: profile })
-            .then(res => res.json())
-            .then(res => {
-                localStorage.setItem('token', JSON.stringify(res.token));
-                localStorage.setItem('username', JSON.stringify(res.name));
-                localStorage.setItem('useremail', JSON.stringify(res.email));
-                // localStorage.setItem('isOwner', true);
-                this.props.history.push('/dashboard');
+	handleLogIn = (e) => {
+		e.preventDefault();
+		let profile = JSON.stringify({
+			email: this.refs.email.value,
+			password: this.refs.password.value
+		});
+		fetch(`/login`, { method: 'POST', headers: { "Content-Type": "application/json" }, body: profile })
+			.then(res => res.json())
+			.then(res => {
+				localStorage.setItem('token', JSON.stringify(res.token));
+				localStorage.setItem('username', JSON.stringify(res.name));
+				localStorage.setItem('useremail', JSON.stringify(res.email));
 
-            })
-            .catch(err => {
-                this.setState({ logged: false })
-                console.log('login error');
-                console.log(err)
-            });
-    }
+				storage.refFromURL(`gs://pokergoit.appspot.com/userpics/${res.email}/${res.name}`)
+					.getDownloadURL()
+					.then((url) => {
+						localStorage.setItem('avatar_url', url);
+						this.props.history.push('/dashboard');
+					});
 
-    render() {
-        return (
-            <main className="login">
-                <section className="login__case">
-                    <LogError condition={this.state.logged} />
-                    <h1 className="login__title">Log in</h1>
-                    <form className="form" action="#">
-                        <label htmlFor="mail" className="form__title">Email</label>
-                        <input id="mail" ref="email" className="form__input" type="mail" name="email" placeholder="email@gmail.com" />
-                        <label htmlFor="passw" className="form__title">Password</label>
-                        <input id="passw" ref="password" className="form__input" type="password" name="password" placeholder="&bull; &bull; &bull; &bull; &bull; &bull;" />
-                        <button className="form__btn" onClick={this.handleLogIn}>Log in</button>
-                    </form>
-                </section>
-            </main>
-        )
-    }
+				// localStorage.setItem('isOwner', true);
+
+
+			})
+			.catch(err => {
+				this.setState({ logged: false })
+				console.log('login error');
+				console.log(err)
+			});
+	}
+
+	render() {
+		return (
+			<main className="login">
+				<section className="login__case">
+					<LogError condition={this.state.logged} />
+					<h1 className="login__title">Log in</h1>
+					<form className="form" action="#">
+						<label htmlFor="mail" className="form__title">Email</label>
+						<input id="mail" ref="email" className="form__input" type="mail" name="email" placeholder="email@gmail.com" />
+						<label htmlFor="passw" className="form__title">Password</label>
+						<input id="passw" ref="password" className="form__input" type="password" name="password" placeholder="&bull; &bull; &bull; &bull; &bull; &bull;" />
+						<button className="form__btn" onClick={this.handleLogIn}>Log in</button>
+					</form>
+				</section>
+			</main>
+		)
+	}
 }
 
 export default withRouter(Login)
